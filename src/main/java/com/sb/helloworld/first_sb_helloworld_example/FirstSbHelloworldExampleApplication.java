5263e70822dcfbe4b271a6cac580663ac265df9f
Sudhalm <sudhalahari145@gmail.com>
2024-07-13 08:28:23 +0530

    my first spring boot program

---
+++ src/main/java/com/sb/helloworld/first_sb_helloworld_example/FirstSbHelloworldExampleApplication.java
@@ -0,0 +1,18 @@
+package com.sb.helloworld.first_sb_helloworld_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSbHelloworldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSbHelloworldExampleApplication.class, args);
+	}
+	@SpringBootApplication
+	public class  FirstSbHelloWorldExampleApplicationTests {
+		public static void main(String[] args) {
+			System.out.println("this is the first spring boot application");
+		}
+	}
+}
