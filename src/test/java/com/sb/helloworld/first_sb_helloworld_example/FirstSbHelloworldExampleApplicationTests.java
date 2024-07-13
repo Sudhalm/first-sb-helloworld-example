4191d031c7659da158f4260b60c4c8f468365dfd
Sudhalm <sudhalahari145@gmail.com>
2024-07-13 12:10:46 +0530

    my first spring boot program

---
+++ src/test/java/com/sb/helloworld/first_sb_helloworld_example/FirstSbHelloworldExampleApplicationTests.java
@@ -0,0 +1,10 @@
+package com.sb.helloworld.first_sb_helloworld_example;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FirstSbHelloworldExampleApplicationTests {
+
+	}
+
+
