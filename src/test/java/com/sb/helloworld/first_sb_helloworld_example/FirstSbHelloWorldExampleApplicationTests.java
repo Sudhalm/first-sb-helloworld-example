8da21bcb49f408df324cbed980b2ac50f53f0bf5
Sudhalm <sudhalahari145@gmail.com>
2024-07-13 08:36:35 +0530

    my first spring boot program

--- src/test/java/com/sb/helloworld/first_sb_helloworld_example/FirstSbHelloWorldExampleApplicationTests.java
+++ src/test/java/com/sb/helloworld/first_sb_helloworld_example/FirstSbHelloWorldExampleApplicationTests.java
@@ -1,14 +1,11 @@
 package com.sb.helloworld.first_sb_helloworld_example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
+@SpringBootTest
 public class  FirstSbHelloWorldExampleApplicationTests{
-	public static void main(String[] args) {
-		System.out.println("hello world");
+
 	}
-}
+
 
